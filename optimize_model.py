d3e46011422e1d3f7d9ae771d33569fd639eada8
Arthur <loboarthur75@gmail.com>
2026-04-16 18:30:38 -0300

    Entrega do desafio técnico - Arthur Lobo

--- optimize_model.py
+++ optimize_model.py
@@ -1,4 +1,18 @@
 import tensorflow as tf
-import os
 
 #insira seu código aqui
+
+#==========================================================================================================
+# Otimizando o modelo
+
+# Carregando o modelo treinado
+model = tf.keras.models.load_model('model.h5')
+
+# Convertendo e aplicando o Dynamic Range Quantization
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+tflite_model = converter.convert()
+
+# Salvando o modelo otimizado
+with open('model.tflite', 'wb') as f:
+    f.write(tflite_model)
